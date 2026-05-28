c6b00a1813c7bc1a3bca8f2fc3aabdbaf3b50b31
Entiharshvardhanrao <entiharshvardhan@gmail.com>
2026-05-27 21:30:39 -0700

    Created Student model and admin integration

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    room_number = models.CharField(max_length=10)
+    department = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
